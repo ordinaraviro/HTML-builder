5d13fc60ae6d515725b489bd7f0e6b2ee943531a
Alexander_Yulin <ordinaraviro@gmail.com>
2022-11-05 22:16:20 +0200

    feat: done 2nd task

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const fs = require('fs');
+
+fs.writeFile(
+    path.join(__dirname, 'text.txt'),
+    '',
+    (err) => {
+        if(err) throw err;
+}
+);
+
+const { stdin, stdout } = process;
+console.log("Салют! Введите текст.")
+stdin.on('data', x => {
+    if(x.toString().trim() == "exit"){
+        process.exit();
+    }
+    fs.appendFile(
+    path.join(__dirname, 'text.txt'),
+    x,
+    (err)=> {
+        if(err) throw err;
+        console.log('Введите еще что-нибудь.')
+    }
+)})
+
+process.on('exit', () => stdout.write('Ввод окончен. Всего доброго.'));
