ba92efc11a81071c2547c750697f1b29df296669
Alexander_Yulin <ordinaraviro@gmail.com>
2022-11-06 20:02:11 +0200

    feat: done 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const fsProms = require('fs/promises');
+const path = require('path');
 
+async function copyFunc() {
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true}, err => {
+        if (err)throw err;
+        console.log('dir created');
+    });
+    const files = await fsProms.readdir(path.join(__dirname, 'files'));
+    for await (let file of files) {
+        const oldPath = path.join(__dirname, 'files', file);
+        const newPath = path.join(__dirname, 'files-copy', file);
+        fs.copyFile(oldPath, newPath, err => {
+            if(err) throw err;
+            console.log(`${file} copied`);
+        });
+    };
+};
+
+fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+    if(err) {
+        console.log("dir not exist")
+        copyFunc();
+    };
+    if(!err){
+        console.log('dir deleted');
+        copyFunc();
+    };
+});
